439143c02c33b9088fb2a571c949cd27c686337c
anthonyharrison <anthony.p.harrison@gmail.com>
2026-03-02 20:34:58 +0000

    refactor: use lib4sbom parser

--- sbomdiff/cli.py
+++ sbomdiff/cli.py
@@ -8,10 +8,10 @@ import sys
 import textwrap
 from collections import ChainMap
 
+from lib4sbom.data.package import SBOMPackage
 from lib4sbom.output import SBOMOutput
+from lib4sbom.parser import SBOMParser
 
-from sbomdiff.cyclonedx_parser import CycloneDXParser
-from sbomdiff.spdx_parser import SPDXParser
 from sbomdiff.version import VERSION
 
 
@@ -35,6 +35,38 @@ def format_package_display(package_key):
     return name
 
 
+def process_packages(package_list):
+    packages = {}
+    thepackage = SBOMPackage()
+    for package in package_list:
+        thepackage.initialise()
+        thepackage.copy_package(package)
+        name = thepackage.get_name()
+        version = thepackage.get_value("version")
+        name = thepackage.get_name()
+        license = thepackage.get_value("licenseconcluded")
+        if license is None:
+            license = "UNKNOWN"
+        checksums = thepackage.get_value("checksum")
+        properties = thepackage.get_value("properties")
+        # Special handling for Syft SBOMs
+        path = ""
+        properties = thepackage.get_value("properties")
+        if properties is not None:
+            for prop in properties:
+                prop_name = prop.get("name", "").lower()
+                # Look for properties with location/path semantics
+                if (
+                    "location" in prop_name and "path" in prop_name
+                ) or prop_name.endswith(":path"):
+                    path = prop.get("value", "")
+                    break
+        package_key = (name, path) if path else (name, "")
+        if package_key not in packages and version is not None:
+            packages[package_key] = [version, license, checksums]
+    return packages
+
+
 # CLI processing
 
 
@@ -42,12 +74,10 @@ def main(argv=None):
     argv = argv or sys.argv
     parser = argparse.ArgumentParser(
         prog="sbomdiff",
-        description=textwrap.dedent(
-            """
+        description=textwrap.dedent("""
             SBOMDiff compares two Software Bill of Materials and
             reports the differences.
-            """
-        ),
+            """),
     )
     input_group = parser.add_argument_group("Input")
     input_group.add_argument(
@@ -61,6 +91,26 @@ def main(argv=None):
         "--exclude-license",
         action="store_true",
         help="suppress reporting differences in the license of components",
+    )
+    input_group.add_argument(
+        "--checksum",
+        action="store",
+        choices=[
+            "MD5",
+            "SHA1",
+            "SHA256",
+            "SHA384",
+            "SHA512",
+            "SHA3-256",
+            "SHA3-384",
+            "SHA3-512",
+            "BLAKE2b-256",
+            "BLAKE2b-384",
+            "BLAKE2b-512",
+            "BLAKE3",
+        ],
+        default="SHA256",
+        help="specify checksum algorithm to use in comparison (default: SHA256)",
     )
     output_group = parser.add_argument_group("Output")
     output_group.add_argument(
@@ -96,6 +146,7 @@ def main(argv=None):
         "exclude_license": False,
         "debug": False,
         "format": "text",
+        "checksum": "SHA256"
     }
     raw_args = parser.parse_args(argv[1:])
     args = {key: value for key, value in vars(raw_args).items() if value}
@@ -118,29 +169,14 @@ def main(argv=None):
         print("Must specify different filenames")
         return -1
 
-    spdx = SPDXParser()
-    cyclonedx = CycloneDXParser()
-
-    if args["sbom"] == "spdx":
-        file1_type = file2_type = "SPDX"
-        packages1 = spdx.parse(args["FILE1"])
-        packages2 = spdx.parse(args["FILE2"])
-    elif args["sbom"] == "cyclonedx":
-        file1_type = file2_type = "CYCLONEDX"
-        packages1 = cyclonedx.parse(args["FILE1"])
-        packages2 = cyclonedx.parse(args["FILE2"])
-    else:
-        # Work out the SBOM type for each file
-        packages1 = spdx.parse(args["FILE1"])
-        file1_type = "SPDX"
-        if len(packages1) == 0:
-            packages1 = cyclonedx.parse(args["FILE1"])
-            file1_type = "CYCLONEDX"
-        packages2 = spdx.parse(args["FILE2"])
-        file2_type = "SPDX"
-        if len(packages2) == 0:
-            file2_type = "CYCLONEDX"
-            packages2 = cyclonedx.parse(args["FILE2"])
+    sbom_parser = SBOMParser(sbom_type=args["sbom"])
+    # Extract packages frome ach file
+    sbom_parser.parse_file(args["FILE1"])
+    packages1 = process_packages(sbom_parser.get_packages())
+    file1_type = sbom_parser.get_type()
+    sbom_parser.parse_file(args["FILE2"])
+    packages2 = process_packages(sbom_parser.get_packages())
+    file2_type = sbom_parser.get_type()
 
     if args["debug"]:
         print("SBOM type", args["sbom"])
@@ -153,12 +189,14 @@ def main(argv=None):
         print("SBOM File2 - type", file2_type)
         print("SBOM File2 - packages", len(packages2))
         print("Exclude Licences", args["exclude_license"])
+        print("Checksum algorithm", args["checksum"])
 
     # Keep count of differences
     version_changes = 0
     new_packages = 0
     removed_packages = 0
     license_changes = 0
+    checksum_changes = 0
 
     sbom_out = SBOMOutput(args["output_file"], args["format"])
 
@@ -171,8 +209,8 @@ def main(argv=None):
         package_name, package_path = package_key
         if package_key in packages2:
             # Compare values for common package
-            version1, license1 = packages1[package_key]
-            version2, license2 = packages2[package_key]
+            version1, license1, checksums1 = packages1[package_key]
+            version2, license2, checksums2 = packages2[package_key]
             version1 = version1.upper()
             version2 = version2.upper()
             package_info["package"] = package_name
@@ -209,9 +247,34 @@ def main(argv=None):
                 package_info["license"] = license_info
                 license_changes += 1
                 diff_record = True
+            if checksums1 is not None and checksums2 is not None:
+                # compare checksums
+                value1 = value2 = None
+                for checksum in checksums1:
+                    if checksum[0] == args["checksum"]:
+                        value1 = checksum[1]
+                        break
+                for checksum in checksums2:
+                    if checksum[0] == args["checksum"]:
+                        value2 = checksum[1]
+                        break
+                if value1 is not None and value2 is not None:
+                    if value1 != value2:
+                        if args["format"] == "text":
+                            sbom_out.send_output(
+                                f"[CHECKSUM] {package_display}: "
+                                f"Checksum changed from {value1} to {value2}"
+                            )
+                        package_info["status"] = "change"
+                        checksum_info = dict()
+                        checksum_info["from"] = value1
+                        checksum_info["to"] = value2
+                        package_info["checksum"] = checksum_info
+                        checksum_changes += 1
+                        diff_record = True
         else:
             # Package must have been removed
-            version1, license1 = packages1[package_key]
+            version1, license1, _ = packages1[package_key]
             version1 = version1.upper()
             package_info["package"] = package_name
             if package_path:
@@ -235,7 +298,7 @@ def main(argv=None):
         if package_key not in packages1:
             package_display = format_package_display(package_key)
             package_name, package_path = package_key
-            version2, license2 = packages2[package_key]
+            version2, license2, _ = packages2[package_key]
             version2 = version2.upper()
             if len(version2) == 0:
                 version2 = "UNKNOWN"
@@ -266,6 +329,7 @@ def main(argv=None):
             sbom_out.send_output(f"License changes:  {license_changes}")
         sbom_out.send_output(f"Removed packages: {removed_packages}")
         sbom_out.send_output(f"New packages:     {new_packages}")
+        sbom_out.send_output(f"Checksum changes: {checksum_changes}")
 
     if args["format"] != "text":
         json_doc = {}
@@ -282,11 +346,12 @@ def main(argv=None):
         summary["removed_packages"] = removed_packages
         if not args["exclude_license"]:
             summary["license_changes"] = license_changes
+        summary["checksum_changes"] = checksum_changes
         json_doc["summary"] = summary
         sbom_out.generate_output(json_doc)
 
     # Return code indicates if any differences have been detected
-    if (version_changes or license_changes or removed_packages or new_packages) != 0:
+    if (version_changes or license_changes or removed_packages or new_packages or checksum_changes) != 0:
         return 1
 
     return 0
